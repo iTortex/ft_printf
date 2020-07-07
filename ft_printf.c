d13fc8c3d4a2e64d10bb26b4d516efecb61c5201
Danila Gruzinow <amarcele@at-m5.msk.21-school.ru>
2020-07-07 20:24:12 +0300

    ntry

--- ft_printf.c
+++ ft_printf.c
@@ -6,68 +6,81 @@
 /*   By: amarcele <amarcele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/03 18:09:19 by amarcele          #+#    #+#             */
-/*   Updated: 2020/07/05 19:10:23 by amarcele         ###   ########.fr       */
+/*   Updated: 2020/07/07 19:54:57 by amarcele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
-void	*workwithid(const char *format, t_print check)
+void	*checkforflags(const char *format, t_print *all, va_list *factor)
 {
-	if (check.first == check.i && format[check.i] == 'i')
+	if (format[all->i] == '.')
 	{
-		check.num = va_arg(check.look, int);
-		ft_putnbr_fd(check.num, 1);
-	}
-	return (0);
-}
-
-void *afterprcnt(const char *format, t_print check)
-{
-	check.i++;
-	check.first = check.i;
-	while (format[check.i] != 'i' || format[check.i] != 'd' || 
-	format[check.i] != 's' || format[check.i] != 'c' || format[check.i] != 'p' ||
-	format[check.i] != 'X' || format[check.i] != 'x'
-	|| format[check.i] != 'p' || format[check.i] != 'u')
-	{
-		check.i++;
-	}
-	if (format[check.i] == 'i' || format[check.i] == 'd')
-		workwithid(format, check);
-	return (0);
-}
-int ft_printf(const char *format, ...)
-{
-	t_print check;
-	
-	check.i = 0;
-	check.first = 0;
-	va_start(check.look, format);
-	while (format[check.i])
-	{
-		if (format[check.i] == '%')
+		char *pnt;
+		pnt = NULL;
+		all->dot = '.';
+		all->i++;
+		all->first = all->i;
+		while (format[all->i] >= '0' && format[all->i] <= '9')
+			all->i++;
+		if (all->i - all->first > 0)
 		{
-			afterprcnt(format, check);
-			// i++;
-			// if (format[i] == 'i' || format[i] == 'd')
-			// 	check.num = va_arg(look, int);
-			// if (format[i] == 's')
-			// 	check.str = va_arg(look, char*);
+			//*pnt = format[all.first];
+			printf("%i\n", ft_atoi(format));
+			all->afterdot = ft_atoi(format);
+			printf("|%i|\n", all->afterdot);
 		}
-		check.i++;
+		if (all->i == '*')
+		{
+			all->star = '*';
+			all->i++;
+			all->afterdot = va_arg(*factor,int);
+		}
 	}
-	return 0;
+	return (0);
 }
 
-
+int		ft_printf(const char *format, ...)
+{
+	t_print all;
+	va_list factor;
+	
+	all.i = 0;
+	all.first = 0;
+	va_start(factor, format);
+	while (format[all.i])
+	{
+		if (format[all.i] == '%')
+		{
+			all.i += 1;
+			if (format[all.i] != 'i' || format[all.i] != 'd' || 
+			format[all.i] != 's' || format[all.i] != 'c' 
+			|| format[all.i] != 'p' || format[all.i] != 'X' 
+			|| format[all.i] != 'x'
+			|| format[all.i] != 'p' || format[all.i] != 'u')
+			{
+				checkforflags(format, &all, &factor);
+			}
+			if (format[all.i] == 's')
+			{
+				all.str = va_arg(factor, char *);
+				if (!all.afterdot)
+					all.afterdot = ft_strlen(all.str);
+				write(1, all.str, all.afterdot);
+			}
+		}
+		all.i++;
+		write(1, &format[all.i], 1);
+	}
+	va_end(factor);
+	return (0);
+}
 
 int main (void)
-{
-	int gg = 123000000;
-	
-	//char *str = "nu chto tam?";
-	ft_printf("%i", gg);
+{	
+	char *str = "nu chto tam?";
+	printf("|%.5saaaaa|\n", str);
+	ft_printf("%.5saaaa", str);
 	return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: amarcele <amarcele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/05 14:17:07 by amarcele          #+#    #+#             */
-/*   Updated: 2020/07/05 18:54:03 by amarcele         ###   ########.fr       */
+/*   Updated: 2020/07/07 19:41:01 by amarcele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@
 
 typedef struct	s_print
 {
-	va_list look;
 	char preob;
-	char *flag;
+	char star;
+	char dot;
 	unsigned int i;
 	unsigned int first;
 	char *type;
@@ -28,9 +28,10 @@ typedef struct	s_print
 	unsigned char sym;
 	unsigned int smallnum;
 	int num;
-	unsigned int afterdot;
+	int afterdot;
 }				t_print;
 
+int	ft_atoi(const char *str);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 size_t	ft_strlen(const char *str);
